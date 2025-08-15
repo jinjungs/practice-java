c2955f3b6b2e7d2651db3f9f9630b2cc269baf12
jinjungs <ejjung.tech@gmail.com>
2025-08-14 20:36:54 -0700

    practice multiple array

--- src/main/java/me/ejjung/array/PracticeArray.java
+++ src/main/java/me/ejjung/array/PracticeArray.java
@@ -13,7 +13,7 @@ public class PracticeArray {
         String[] arr2;
         // arr2 = {"Amy", "Susan", "Mark"}; // compile error!
 
-        /* 1.2. Using new */
+        /* 1.2. Using new and size of array */
         String[] arr3 = new String[3];
 
         // You can use create array later, unlike just using values in 1.1.
@@ -22,5 +22,20 @@ public class PracticeArray {
 
         // Store new value in array after declare using index
         arr3[1] = "Hana";
+
+        /**
+         * 2. Multidimensional Array
+         */
+
+        /* 2.1. Using values */
+        int[][] array = { {1,2,3}, {4,5,6} };
+
+        /* 2.2. Using new and size of array */
+        int[][] scores = new int[2][3];
+
+        /* 2.3. Stair-like array */
+        int[][] stairs = new int[2][];
+        stairs[0] = new int[2];
+        stairs[1] = new int[3];
     }
 }
