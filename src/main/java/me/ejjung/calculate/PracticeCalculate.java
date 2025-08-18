6a799b8950b05ae01177b0c8e377fef2c6fe139d
jinjungs <ejjung.tech@gmail.com>
2025-08-18 14:29:33 -0700

    practice calculate ++

---
+++ src/main/java/me/ejjung/calculate/PracticeCalculate.java
@@ -0,0 +1,14 @@
+package me.ejjung.calculate;
+
+public class PracticeCalculate {
+    public static void main(String[] args) {
+        int x = 1;
+        int y = 1;
+        int result1 = (++x) + 10;
+        int result2 = (y++) + 10;
+
+        System.out.println(result1);
+        System.out.println(result2);
+    }
+
+}
